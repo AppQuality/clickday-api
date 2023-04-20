f97f7e1cc271313634f5812f5bcd9a1f52af0620
Davide Bizzi <davide.bizzi@unguess.io>
2023-04-20 09:17:14 +0200

    rework: Refactor get attempts

--- src/routes/attempts/index.ts
+++ src/routes/attempts/index.ts
@@ -1,6 +1,5 @@
 /** OPENAPI-CLASS: get-attempts */
 
-import OpenapiError from "@src/features/OpenapiError";
 import { clickDay } from "@src/features/database";
 import UserRoute from "@src/features/routes/UserRoute";
 
@@ -8,61 +7,36 @@ export default class Route extends UserRoute<{
   response: StoplightOperations["get-attempts"]["responses"]["200"]["content"]["application/json"];
 }> {
   protected async prepare(): Promise<void> {
-    try {
-      const attempts = await this.getAttempts();
-
-      return this.setSuccess(200, this.mapAttempts(attempts));
-    } catch (e) {
-      return this.setError(
-        404,
-        new OpenapiError("There are no attempts for this tester")
-      );
-    }
+    const attempts = await this.getAttempts();
+    return this.setSuccess(200, attempts);
   }
 
-  public async getAttempts() {
+  private async getAttempts() {
     const attempts = await clickDay.tables.CdAttempts.do()
       .select("id", "agency_code", "start_time", "end_time", "errors")
       .where({
         tester_id: this.getTesterId(),
       });
-    if (!attempts) {
-      return [];
-    }
-    return attempts;
+    return attempts.map((attempt) => ({
+      id: attempt.id,
+      date: this.formatStartDate(attempt.start_time),
+      time: this.getElapsedTime(attempt).toString(),
+      code: attempt.agency_code,
+      errors: attempt.errors ? attempt.errors : 0,
+    }));
   }
 
-  private mapAttempts(
-    attempts: Awaited<
-      Promise<
-        {
-          id: number;
-          agency_code: string;
-          start_time: string;
-          end_time: string | null;
-          errors: number | null;
-        }[]
-      >
-    >
-  ) {
-    return attempts.map((attempt) => {
-      const startDate = new Date(attempt.start_time);
-      const startDateString = `${startDate.getFullYear()}-${
-        startDate.getMonth() + 1
-      }-${startDate.getDate()}`;
+  private formatStartDate(date: string) {
+    const startDate = new Date(date);
+    return `${startDate.getFullYear()}-${
+      startDate.getMonth() + 1
+    }-${startDate.getDate()}`;
+  }
 
-      const startTime = new Date(attempt.start_time).getTime();
-      const endTime = attempt.end_time
-        ? new Date(attempt.end_time).getTime()
-        : 0;
-
-      return {
-        id: attempt.id,
-        date: startDateString,
-        time: (attempt.end_time ? endTime - startTime : 0).toString(),
-        code: attempt.agency_code,
-        errors: attempt.errors ? Number(attempt.errors) : 0,
-      };
-    });
+  private getElapsedTime(attempt: { start_time: string; end_time?: string }) {
+    if (!attempt.end_time) return 0;
+    const startDate = new Date(attempt.start_time);
+    const endDate = new Date(attempt.end_time);
+    return endDate.getTime() - startDate.getTime();
   }
 }
