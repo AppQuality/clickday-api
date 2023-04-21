2c37882cbc58e5ce77f52a4e392043ba7ea06fa2
Gianpaolo Sinatra <gianpaolo.sinatra@app-quality.com>
2023-04-21 16:08:45 +0200

    test(get-attempts): Should return attempts ordered by start_date desc

--- src/routes/attempts/_get/index.spec.ts
+++ src/routes/attempts/_get/index.spec.ts
@@ -24,39 +24,45 @@ describe("GET /attempts tester has no attempts", () => {
 
 describe("GET /attempts", () => {
   beforeAll(async () => {
-    const start = new Date();
-    start.setHours(0, 0, 0, 0);
-    const end = new Date();
-    end.setHours(0, 0, 0, 10);
+    const startToday = new Date();
+    startToday.setHours(0, 0, 0, 0);
+    const endToday = new Date();
+    endToday.setHours(0, 0, 0, 10);
+    const startYesterday = new Date();
+    startYesterday.setDate(startYesterday.getDate() - 1);
+    startYesterday.setHours(0, 0, 0, 0);
+    const endYesterday = new Date();
+    endYesterday.setDate(endYesterday.getDate() - 1);
+    endYesterday.setHours(0, 0, 0, 10);
     await clickDay.tables.CdAttempts.do().insert([
       {
         id: 1,
         tester_id: 1,
         agency_code: "+asd123",
-        start_time: start.toISOString(),
-        end_time: end.toISOString(),
+        start_time: startYesterday.toISOString(),
+        end_time: endYesterday.toISOString(),
         errors: 0,
       },
       {
         id: 2,
         tester_id: 1,
         agency_code: "+asd123",
-        start_time: start.toISOString(),
-        end_time: end.toISOString(),
+        start_time: startToday.toISOString(),
+        end_time: endToday.toISOString(),
         errors: 4,
       },
       {
         id: 3,
         tester_id: 1,
         agency_code: "+asd123",
-        start_time: start.toISOString(),
+        start_time: startToday.toISOString(),
       },
       {
         id: 4,
         tester_id: 123,
         agency_code: "-asd123",
-        start_time: start.toISOString(),
-        end_time: end.toISOString(),
+        start_time: startToday.toISOString(),
+        end_time: endToday.toISOString(),
         errors: 0,
       },
     ]);
@@ -72,21 +78,26 @@ describe("GET /attempts", () => {
     const todayString = `${today.getFullYear()}-${
       today.getMonth() + 1
     }-${today.getDate()}`;
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    const yesterdayString = `${yesterday.getFullYear()}-${
+      yesterday.getMonth() + 1
+    }-${yesterday.getDate()}`;
 
     expect(response.body).toEqual([
-      {
-        id: 1,
-        date: todayString,
-        time: "10",
-        code: "+asd123",
-        errors: 0,
-      },
       {
         id: 2,
         date: todayString,
         time: "10",
         code: "+asd123",
         errors: 4,
+      },
+      {
+        id: 1,
+        date: yesterdayString,
+        time: "10",
+        code: "+asd123",
+        errors: 0,
       },
     ]);
   });
@@ -101,7 +112,6 @@ describe("GET /attempts", () => {
     const response = await request(app)
       .get("/attempts")
       .set("authorization", "Bearer tester");
-    console.log(response.body);
     expect(response.status).toBe(200);
     expect(response.body.length).toBe(comletedAttempts.length);
     const today = new Date();
@@ -109,15 +119,13 @@ describe("GET /attempts", () => {
       today.getMonth() + 1
     }-${today.getDate()}`;
 
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    const yesterdayString = `${yesterday.getFullYear()}-${
+      yesterday.getMonth() + 1
+    }-${yesterday.getDate()}`;
     expect(response.body.length).not.toEqual(attempts.length);
     expect(response.body).toEqual([
-      {
-        id: 1,
-        date: todayString,
-        time: "10",
-        code: "+asd123",
-        errors: 0,
-      },
       {
         id: 2,
         date: todayString,
@@ -125,6 +133,24 @@ describe("GET /attempts", () => {
         code: "+asd123",
         errors: 4,
       },
+      {
+        id: 1,
+        date: yesterdayString,
+        time: "10",
+        code: "+asd123",
+        errors: 0,
+      },
     ]);
+  });
+
+  //Should return attempts ordered by start_date desc
+  it("Should return attempts ordered by start_date desc", async () => {
+    const response = await request(app)
+      .get("/attempts")
+      .set("authorization", "Bearer tester");
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(2);
+    expect(response.body[0].id).toBe(2);
+    expect(response.body[1].id).toBe(1);
   });
 });
