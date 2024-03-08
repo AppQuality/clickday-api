54bbec8e0d9036af4a46fb3926ba1adefe31dfc6
it@app-quality.com <it@app-quality.com>
2024-03-08 15:20:03 +0100

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwOTU3Ng/spectral.js?branch_slug=events-post&token=e38bcb54-e42f-4542-984b-bdd40c1717f7";
+export default { extends: ruleset };
