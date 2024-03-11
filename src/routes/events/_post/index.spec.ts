3cce80b42b7825a277e1ddfdc7d1a3890db247b8
ZecD <dominik.zecoli@gmail.com>
2024-03-11 15:48:38 +0100

    added additional test to events-post

--- src/routes/events/_post/index.spec.ts
+++ src/routes/events/_post/index.spec.ts
@@ -167,6 +167,35 @@ describe("POST /events", () => {
     expect(questions.length).toBe(9);
   });
 
+  it("Should have all the expected question fields", async () => {
+    const response = await request(app)
+      .post("/events")
+      .send({
+        title: "Test icolo 2",
+        start_date: "2021-08-01T00:00:00.000Z",
+        end_date: "2021-09-01T00:00:00.000Z",
+      })
+      .set("authorization", "Bearer admin");
+    expect(response.status).toBe(200);
+    const eventToAttempt = await clickDay.tables.CdEventsToAttempts.do()
+      .select()
+      .where("event_id", response.body.id);
+    const attempt = await clickDay.tables.CdAttempts.do().select().where({
+      id: eventToAttempt[0].attempt_id,
+    });
+    const questions = await clickDay.tables.CdAttemptsQuestions.do()
+      .select()
+      .where("attempt_id", attempt[0].id);
+    for (const question of questions) {
+      expect(question.title).toBeDefined();
+      expect(question.type).toBeDefined();
+      expect(question.correct_answer).toBeDefined();
+      if (question.input_type === "dropdown") {
+        expect(question.options).toBeDefined();
+      }
+    }
+  });
+
   it("Should fail if the user is not an administator", async () => {
     const response = await request(app)
       .post("/events")
