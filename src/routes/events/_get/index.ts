5fba859be41ff39c61edb7d8f9aaba694c8a1458
Marco Bonomo <marco.bonomo@unguess.io>
2024-03-12 09:50:51 +0100

    🔧 fix(events): convert start_date and end_date to string in response data

--- src/routes/events/_get/index.ts
+++ src/routes/events/_get/index.ts
@@ -12,8 +12,8 @@ export default class Route extends UserRoute<{
     return this.setSuccess(200, {
       id: event.id,
       title: event.title,
-      start_date: event.start_date,
-      end_date: event.end_date,
+      start_date: event.start_date.toString(),
+      end_date: event.end_date.toString(),
     });
   }
 
