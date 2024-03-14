0aabd8e0fac698a0333db4032f892bbd3f97968f
Marco Bonomo <marco.bonomo@unguess.io>
2024-03-14 15:23:35 +0100

    🔨 refactor(CodeNoSymbolQuestion.ts): refactor CodeNoSymbolQuestion class 🚀 feat(CodeNoSymbolQuestion.ts): add method to add options dynamically

--- src/routes/attempts/_post/questions/v2/CodeNoSymbolQuestion.ts
+++ src/routes/attempts/_post/questions/v2/CodeNoSymbolQuestion.ts
@@ -1,7 +1,7 @@
 import DropdownQuestion from "./DropdownQuestion";
 
 export default class CodeNoSymbolQuestion extends DropdownQuestion<"code-no-symbol-v2"> {
-  public static currentOptions = [
+  private static readonly options = [
     "%cb92246a-0634-4b94-8473-a8503bee1962NonSelezionareQuestoToken0101",
     "(78d636ec-0737-45c6-8868-b1fbNonSelezionareQuestoToken01010836478b",
     ")7028b564-8a61-4a7NonSelezionareQuestoToken01011-995e-ec63607479df",
@@ -10,19 +10,14 @@ export default class CodeNoSymbolQuestion extends DropdownQuestion<"code-no-symb
     "?NonSelezionareQuestoToken01010b34aeb9-24be-49fb-9c05-727d7ef74d36",
     "!1cf6999f-1364-4ace-b2a0NonSelezionareQuestoToken0101-377989e72b62",
   ];
-  public static correctIndex = Math.floor(
-    Math.random() * CodeNoSymbolQuestion.currentOptions.length
-  );
 
   constructor(code: string) {
+    CodeNoSymbolQuestion.addOption(code);
     super(
       "code-no-symbol-v2",
       `il codice identificativo senza considerare il "+" (più) o il "-" (meno)`,
-      CodeNoSymbolQuestion.currentOptions.splice(
-        CodeNoSymbolQuestion.correctIndex,
-        0,
-        code.replace(/\+|-/g, "")
-      )
+      CodeNoSymbolQuestion.options,
+      { code }
     );
   }
 
@@ -30,9 +25,21 @@ export default class CodeNoSymbolQuestion extends DropdownQuestion<"code-no-symb
     return 4;
   }
 
-  protected getCorrect(args: Record<string, any> = {}) {
-    return CodeNoSymbolQuestion.currentOptions[
-      CodeNoSymbolQuestion.correctIndex
-    ];
+  protected getCorrect({ code }: { code: string }) {
+    const correctOption = this.currentOptions.find(
+      (option) => option === code.replace(/\+|-/g, "")
+    );
+    if (!correctOption) {
+      throw new Error("Correct option no symbol not found");
+    }
+    return correctOption;
+  }
+
+  static addOption(code: string) {
+    CodeNoSymbolQuestion.options.splice(
+      Math.floor(Math.random() * CodeNoSymbolQuestion.options.length),
+      0,
+      code.replace(/\+|-/g, "")
+    );
   }
 }
